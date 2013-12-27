018deaf30e4542425a8ad33c1c3cbafaecf5e1ad
paullewallencom <paul.lewallen@gmail.com>
2013-12-27 14:58:58 -0800

    Connect to local database

--- header.php
+++ header.php
@@ -1,5 +1,11 @@
 <?php
 require("config.php");
+
+$db = mysql_connect($dbhost, $dbuser, $dbpassword);
+mysql_select_db($dbdatabase, $db);
+
+
+
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
         "http://www.w3.org/TR/html4/loose.dtd">
