f455b683a3be8b7038ba50318b6aaa787d710f51
paullewallencom <paul.lewallen@gmail.com>
2013-12-27 09:50:28 -0800

    Initial rep configuration

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+   $dbhost = "localhost";
+   
+   $dbuser = "root";
+   
+   $dbpassword = "";
+   
+   $dbdatabase = "blogtastic-12-19-2013";
+   
+   $config_blogname = "Grammatic goo";
+   
+   $config_author = "Paul Lewallen";
+   
+   $config_basedir = "http://127.0.0.1/_SandBox/weblog/";
+   
+?>
