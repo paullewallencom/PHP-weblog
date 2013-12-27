6ead22f08a654d535f8bc2dbaa88657936924fb9
paullewallencom <paul.lewallen@gmail.com>
2013-12-27 10:17:40 -0800

    Initial header.php commit

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+require("config.php");
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title><?php echo $config_blogname; ?></title>
+    <link rel="stylesheet" href="css/stylesheet.css" type="text/css" />
+</head>
+<body>
+<div id="header">
+    <h1><?php echo $config_blogname; ?></h1>
+    [<a href="index.php">home</a>]
+</div>
+<div id="main">
