67044e2207a138cbb21f48d40078b7ebe7c36533
paullewallencom <paul.lewallen@gmail.com>
2013-12-27 15:02:35 -0800

    Successfully displayed data

--- index.php
+++ index.php
@@ -7,6 +7,17 @@ $sql = "SELECT entries.*, categories.cat FROM entries, categories
         LIMIT 1;";
 $result = mysql_query($sql);
 $row = mysql_fetch_assoc($result);
+echo "<h2><a href='viewentry.php?id=" . $row['id']
+        . "'>" . $row['subject'] .
+        "</a></h2><br />";
+echo "<i>In <a href='viewcat.php?id=" . $row['cat_id']
+        ."'>" . $row['cat'] .
+        "</a> - Posted on " . date("D jS F Y g.iA",
+                strtotime($row['dateposted'])) .
+        "</i>";
+echo "<p>";
+echo nl2br($row['body']);
+echo "</p>";
 
 require("footer.php");
 ?>
