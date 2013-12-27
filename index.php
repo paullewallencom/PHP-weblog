d402212e6812070ec69e6675cba6837d4d9cc7e4
paullewallencom <paul.lewallen@gmail.com>
2013-12-27 14:30:59 -0800

    SQL query last entry that was added to the database.
    SQL query - Return only the last entry that was added to the database.

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 require("header.php");
+
+$sql = "SELECT entries.*, categories.cat FROM entries, categories
+WHERE entries.cat_id = categories.id
+ORDER BY dateposted DESC
+LIMIT 1;";
+$result = mysql_query($sql);
+$row = mysql_fetch_assoc($result);
+
 require("footer.php");
 ?>
