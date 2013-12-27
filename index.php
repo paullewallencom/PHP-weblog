f0ed5a9ef96954931709810779c43ed895007cac
paullewallencom <paul.lewallen@gmail.com>
2013-12-27 15:04:40 -0800

    Add comment display

--- index.php
+++ index.php
@@ -18,6 +18,23 @@ echo "<i>In <a href='viewcat.php?id=" . $row['cat_id']
 echo "<p>";
 echo nl2br($row['body']);
 echo "</p>";
-
+echo "<p>";
+$commsql = "SELECT name FROM comments WHERE blog_id = " . $row['id'] .
+        " ORDER BY dateposted;";
+$commresult = mysql_query($commsql);
+$numrows_comm = mysql_num_rows($commresult);
+if($numrows_comm == 0) {
+    echo "<p>No comments.</p>";
+}
+else {
+    echo "(<strong>" . $numrows_comm . "</strong>) comments : ";
+    $i = 1;
+    while($commrow = mysql_fetch_assoc($commresult)) {
+        echo "<a href='viewentry.php?id=" . $row['id'] ."#comment" . $i .
+                "'>" . $commrow['name'] . "</a> ";
+        $i++;
+    }
+}
+echo "</p>";
 require("footer.php");
 ?>
