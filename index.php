8701c236e1a029cfc89039a19b3dc8601b8a6fcc
paullewallencom <paul.lewallen@gmail.com>
2013-12-27 14:40:37 -0800

    Output results of query details

--- index.php
+++ index.php
@@ -13,5 +13,13 @@ echo "<h2><a href='viewentry.php?id=" . $row['id']
         "</a></h2><br />";
 echo "<i>In <a href='viewcat.php?id=" . $row['cat_id']
 
+        ."'>" . $row['cat'] .
+        "</a> - Posted on " . date("D jS F Y g.iA",
+                strtotime($row['dateposted'])) .
+        "</i>";
+echo "<p>";
+echo nl2br($row['body']);
+echo "</p>";
+
 require("footer.php");
 ?>
